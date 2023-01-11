748a63564a389fb88262e94a186083c4f6e90219
emre-serdar <emreserdaar@gmail.com>
2023-01-11 11:23:05 -0500

    Project is done.

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #default True, to use less memory
+
 
 db=SQLAlchemy(app)
+
 
 class Todo(db.Model): #todo table's model
     task_id=db.Column(db.Integer, primary_key=True) #assigning unique value for each task
     name=db.Column(db.String(100)) #getting the task name allowed up to 100 chars
+    due=db.Column(db.String(100))
     
 @app.route('/') # home page route
 def home():
-    todo_list=Todo.query.all()
+    todo_list=Todo.query.order_by(Todo.due.asc()).all() #ordering tasks according to due dates
     return render_template('index.html', todo_list=todo_list) #rendering html and passing the data in table to that html template
 
 @app.route('/add', methods=['POST'])
 def add():
     name=request.form.get("name")
-    new_task=Todo(name=name) #creating new task, task id will be uniquely generated.
+    due=request.form.get("duedate")
+    new_task=Todo(name=name, due=due) #creating new task, task id will be uniquely generated.
     db.session.add(new_task) #adding to database
     db.session.commit()
     return redirect(url_for("home")) #returning to the home page
